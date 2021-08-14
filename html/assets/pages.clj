e1876d8649d9222c94d7f836916bdfc0f0076a56
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-08-14 11:34:39 -0400

    clean up naming

--- html/assets/pages.clj
+++ html/assets/pages.clj
@@ -7,11 +7,11 @@
     [reitit.ring.middleware.parameters :as parameters]
     [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]))
 
-(defn wrap-site-defaults []
+(defn wrap-page-defaults []
   (let [error-page (layout/error-page
                      {:status 403
                       :title "Invalid anti-forgery token"})]
-    #(wrap-anti-forgery % {:error-response error-page}))))
+    #(wrap-anti-forgery % {:error-response error-page})))
 
 (defn home [request]
   (layout/render request "home.html"))
@@ -24,8 +24,8 @@
 (defn route-data
   [{:keys [template-opts]}]
   {:swagger    {:id ::api}
-   :middleware [;; Ring site defaults
-                (wrap-site-defaults)
+   :middleware [;; Default middleware for pages
+                (wrap-page-defaults)
                 ;; query-params & form-params
                 parameters/parameters-middleware
                 ;; encoding response body
