ea4a22ad4a21ee2e2ae51677bfb0ba40d18cf2ac
Dan Boykis <danboykis@gmail.com>
2021-09-27 14:03:39 -0400

    a sketch of cljs module

---
+++ cljs/assets/src/core.cljs/com/danboykis/test/core.cljs
@@ -0,0 +1,19 @@
+(ns com.danboykis.test.core
+    (:require
+      [reagent.core :as r]
+      [reagent.dom :as d]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Welcome to Reagent!!!!!!!!!!!!!"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
