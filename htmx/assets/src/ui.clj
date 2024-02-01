d6530d4e94782676c93843d314e4d9722a0f7b4e
Marko Kocić <marko@euptera.com>
2024-02-01 13:40:20 +0100

    Bump htmx and hyperscript version

--- htmx/assets/src/ui.clj
+++ htmx/assets/src/ui.clj
@@ -13,8 +13,8 @@
    [:head
     [:meta {:charset "UTF-8"}]
     [:title "Htmx + Kit"]
-    [:script {:src "https://unpkg.com/htmx.org@1.7.0/dist/htmx.min.js" :defer true}]
-    [:script {:src "https://unpkg.com/hyperscript.org@0.9.5" :defer true}]]
+    [:script {:src "https://unpkg.com/htmx.org@1.9.10/dist/htmx.min.js" :defer true}]
+    [:script {:src "https://unpkg.com/hyperscript.org@0.9.12" :defer true}]]
    [:body
     [:h1 "Welcome to Htmx + Kit module"]
     [:button {:hx-post "/clicked" :hx-swap "outerHTML"} "Click me!"]]))
